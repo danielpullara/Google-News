0c3025cbb0df85d312f0ad2f678f75306ca40c14
dannyp <danielpullara92@gmail.com>
2020-03-12 15:24:47 +0700

    sixth commit

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 let newsList = [];
 let sourceTitle = {};
 let page = 1
-
+let apiKey = `c74225add6af4f70b0edb124c26f779e`
 
 let callAPI = async () => {
-    let apiKey = `c74225add6af4f70b0edb124c26f779e`
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${apiKey}&page=${page}`
-    
     let data = await fetch(url);
     let result = await data.json();
     console.log("data", data);
@@ -17,13 +15,55 @@ let callAPI = async () => {
     console.log('json', result);
     console.log('article list', newsList);
 
-    // nameTitle(newsList);
-
     render(newsList)
 
 }
+
+let searchByCategory = async () => {
+    let category = document.getElementById("category").value;
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${apiKey}&page=${page}`
+    let data = await fetch(url);
+    let result = await data.json();
+    
+    newsList = result.articles;
+    render(newsList);
+};
+
+let searchBySource = () => {
+    let sourceNames = newsList.map(item => item.source.name);
+
+    let sourceObject = sourceNames.reduce((total, name) => {
+        console.log("total:", total);
+        if (name in total) {
+            total[name]++;
+        } else {
+            total[name] = 1;
+        }
+        return total;
+    }, {});
+
+    let sourceArray = Object.keys(sourceObject);
+
+    let htmlForSource = sourceArray.map(
+        item =>
+            `<input onchange='sourceClicked("${item}")' type="checkbox" id="${item}"/> ${item} (${sourceObject[item]})`
+    );
+
+    document.getElementById("sourceArea").innerHTML = htmlForSource;
+};
+
+// let sourceClicked = index => {
+//     if (document.getElementById(index).checked == true) {
+//         let filteredNews = newsList.filter(item => item.source.name === index);
+//         render(filteredNews);
+//     } else {
+//         render(newsList);
+//     }
+// };
+
+
 let render = (array) => {
-    let htmlForNews = array.map((item,index) =>{
+    let htmlForNews = array.map((item, index) => {
         return `<div id="news" style="display: flex; border: 1px solid grey">
         <img style="width: 200px;"
         src="${item.urlToImage}"
@@ -34,20 +74,20 @@ let render = (array) => {
             <a href="${item.url}">Get Full Coverage</a> 
              <div>${item.description}</div>
              <div>${moment(item.publishedAt, "YYYYMMDD").fromNow()}</div>
-            <p>${index+1} of ${array.length}</p>
+            <p>${index + 1} of ${array.length}</p>
         </div>   
     </div> 
     `;
     }).join('')
 
-document.getElementById('newsArea').innerHTML = htmlForNews
+    document.getElementById('newsArea').innerHTML = htmlForNews;
 }
 
 
 callAPI()
 
-function seemore(){
-    page = page +1 ;
+function seemore() {
+    page = page + 1;
     callAPI();
 }
 
